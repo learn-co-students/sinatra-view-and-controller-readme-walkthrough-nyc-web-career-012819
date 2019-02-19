1da2c60ee9690c10417f5e147b8e9209d69556e2
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-02-19 17:25:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,14 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    regular_string = params[:string]
+    @reversed_string = regular_string.reverse
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
+    erb :friends
   end
 end
